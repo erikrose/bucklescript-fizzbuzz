c23accc31c69d885f7c5ee043ddb52c3037b061d
Vladimir Keleshev <vladimir@keleshev.com>
2017-08-15 15:11:05 +0200

    Another version of FizzBuzz for comparison

--- src/fizzbuzz.ml
+++ src/fizzbuzz.ml
@@ -1,12 +1,11 @@
-let rec range a b =
-  if a > b then [] else a :: (range (a+1) b)
-let fizzy_string num =
-  let three = (num mod 3 = 0) in
-  let five = (num mod 5 = 0) in
-  if three && five then "FizzBuzz" else
-    if three then "Fizz" else
-      if five then "Buzz" else
-        string_of_int num
-let main top =
-  (List.iter (fun num -> Js.log (fizzy_string num)) (range 1 top))
-let () = main 100
+let to_string n =
+  match n mod 3, n mod 5 with
+  | 0, 0 -> "FizzBuzz"
+  | 0, _ -> "Fizz"
+  | _, 0 -> "Buzz"
+  | _, _ -> string_of_int n
+
+let () =
+  for i = 1 to 100 do
+    print_endline (to_string i)
+  done
